426be74bc35d38edcffaf7406b93fdebb2adc000
KEWYN <kirill@withcontext.co>
2024-12-05 16:39:57 +0400

    react refresh and hot module replacement plugins webpack

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -2,12 +2,13 @@ import HtmlWebpackPlugin from 'html-webpack-plugin'
 import webpack from 'webpack'
 import { BuildOptions } from './types/config'
 import MiniCssExtractPlugin from 'mini-css-extract-plugin'
+import ReactRefreshPlugin from '@pmmmwh/react-refresh-webpack-plugin'
 
 export function buildPlugins({
   paths,
   isDev,
 }: BuildOptions): webpack.WebpackPluginInstance[] {
-  return [
+  const plugins = [
     new HtmlWebpackPlugin({
       template: paths.html,
     }),
@@ -20,4 +21,11 @@ export function buildPlugins({
       __IS_DEV__: JSON.stringify(isDev),
     }),
   ]
+
+  if (isDev) {
+    plugins.push(new ReactRefreshPlugin())
+    plugins.push(new webpack.HotModuleReplacementPlugin())
+  }
+
+  return plugins
 }
