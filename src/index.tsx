744674f1a771916d4eeed20c08399c203dfef037
KEWYN <kirill@withcontext.co>
2024-12-06 13:20:24 +0300

    error boundary, add run prod command

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import { render } from 'react-dom'
 import { BrowserRouter } from 'react-router-dom'
 import { AppRouter } from 'app/providers/router'
+import ErrorBoundary from 'app/providers/ErrorBoundary/ui/ErrorBoundary'
 import App from './app/App'
 import ThemeProvider from './app/providers/ThemeProvider/ui/ThemeProvider'
 
@@ -8,11 +9,13 @@ import 'shared/config/i18n/i18n'
 
 render(
     <BrowserRouter>
-        <ThemeProvider>
-            <App>
-                <AppRouter />
-            </App>
-        </ThemeProvider>
+        <ErrorBoundary>
+            <ThemeProvider>
+                <App>
+                    <AppRouter />
+                </App>
+            </ThemeProvider>
+        </ErrorBoundary>
     </BrowserRouter>,
     document.getElementById('root')
 )
