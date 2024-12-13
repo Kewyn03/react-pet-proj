b2aeeb3ebf38624cdf505a41f9d51783d1bf5a2a
Kewyn03 <kirilltichonov@yandex.com>
2024-12-13 20:03:52 +0300

    fix storybook build

--- src/shared/config/storybook/RouterDecorator/RouterDecorator.tsx
+++ src/shared/config/storybook/RouterDecorator/RouterDecorator.tsx
@@ -1,5 +1,9 @@
 import 'app/styles/index.scss'
 import { Story } from '@storybook/react'
 import { BrowserRouter } from 'react-router-dom'
+import { USER_LOCAL_STORAGE_KEY } from 'shared/const/localstorage'
 
-export const RouterDecorator = (story: () => Story) => <BrowserRouter>{story()}</BrowserRouter>
+export const RouterDecorator = (story: () => Story) => {
+    window.localStorage.setItem(USER_LOCAL_STORAGE_KEY, 'asd')
+    return <BrowserRouter>{story()}</BrowserRouter>
+}
